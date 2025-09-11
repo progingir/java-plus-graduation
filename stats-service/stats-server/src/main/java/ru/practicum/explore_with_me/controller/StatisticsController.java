2911f955c2f4d0a4be53cc61db808ce984f2b3d3
Valerii_Butko <lb2005lba@gmail.com>
2025-09-11 11:49:27 +0500

    перехватываю выброс illegalargumentexception и возвращаю статус 400, как и требую тесты

--- stats-service/stats-server/src/main/java/ru/practicum/explore_with_me/controller/StatisticsController.java
+++ stats-service/stats-server/src/main/java/ru/practicum/explore_with_me/controller/StatisticsController.java
@@ -4,7 +4,8 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.practicum.dto.stats.GetResponse;
 import ru.practicum.dto.stats.HitRequest;
 import ru.practicum.feign.StatsClient;
@@ -12,6 +13,7 @@ import ru.practicum.explore_with_me.service.StatisticsService;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -21,14 +23,27 @@ public class StatisticsController implements StatsClient {
     final StatisticsService statisticsService;
 
     @Override
-    public void addHit(HitRequest hitRequest) {
+    @PostMapping("/hit")
+    public void addHit(@RequestBody HitRequest hitRequest) {
         log.info("Post request to /hit");
         statisticsService.createHit(hitRequest);
     }
 
     @Override
-    public List<GetResponse> getStatistics(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+    @GetMapping("/stats")
+    public List<GetResponse> getStatistics(
+            @RequestParam LocalDateTime start,
+            @RequestParam LocalDateTime end,
+            @RequestParam(required = false) List<String> uris,
+            @RequestParam(defaultValue = "false") boolean unique) {
         log.info("Get request to /stats");
         return statisticsService.getStatistics(start, end, uris, unique);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error("Ошибка валидации: {}", ex.getMessage());
+        return Map.of("error", "Неверные параметры запроса", "message", ex.getMessage());
     }
 }
