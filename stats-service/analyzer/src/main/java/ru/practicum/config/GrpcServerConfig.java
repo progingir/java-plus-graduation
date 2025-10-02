243f000ffd63442f36f52284abb9abe9591a8688
Valerii_Butko <lb2005lba@gmail.com>
2025-10-03 03:15:59 +0500

    пробуююююю

---
+++ stats-service/analyzer/src/main/java/ru/practicum/config/GrpcServerConfig.java
@@ -0,0 +1,12 @@
+package ru.practicum.config;
+
+import net.devh.boot.grpc.server.config.GrpcServerProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GrpcServerConfig {
+
+    public GrpcServerConfig(GrpcServerProperties grpcServerProperties) {
+        grpcServerProperties.setPort(9097); // Устанавливаем порт явно
+    }
+}
